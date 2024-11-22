dbe3571f5250d2e6d5339d5fbdd266c350392d24
JerryLong <172066912@qq.com>
2024-11-22 15:07:05 +0800

    fix: 统一改成org

--- trace-spring/src/test/java/org/jrl/trace/TestApplication.java
+++ trace-spring/src/test/java/org/jrl/trace/TestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, MongoAutoConfiguration.class}, scanBasePackages = {"com.jrl"})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, MongoAutoConfiguration.class}, scanBasePackages = {"org.jrl"})
 public class TestApplication {
 
     public static void main(String[] args) {
